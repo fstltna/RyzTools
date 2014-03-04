8bd41290d2535ca951691a100b2faacff889707e
Marisa Giancarla <fstltna@me.com>
2014-03-03 22:08:25 -0800

    Bump version number

--- js/pages.js
+++ js/pages.js
@@ -2,7 +2,7 @@
 |                          Index                             |
 \************************************************************/
 var loadIndexPage = function () {
-	$('#indexPage .article').html('<h1>Ryztools v 0.4.0</h1>\
+	$('#indexPage .article').html('<h1>Ryztools v 0.5.0</h1>\
 	<p>\
 		Welcome to Ryztools.\
 	</p>');
